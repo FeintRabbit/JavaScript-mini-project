9528ea37cc8bbb2f692bb5a5c0f5bee5a829c841
Andrew <acasta4704@gmail.com>
2020-09-05 19:13:18 +1000

    project 4 complete

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// set nav button (nav-toggle) to
+// turn on/off the show-links class for the links ul
+
+const navBtn = document.querySelector(".nav-toggle");
+const linksNav = document.querySelector(".links");
+
+// navBtn.addEventListener("click", () => {
+//   if (linksNav.classList.contains("show-links")) {
+//     linksNav.classList.remove("show-links");
+//   } else {
+//     linksNav.classList.add("show-links");
+//   }
+// });
+
+navBtn.addEventListener("click", () => {
+  linksNav.classList.toggle("show-links");
+});
