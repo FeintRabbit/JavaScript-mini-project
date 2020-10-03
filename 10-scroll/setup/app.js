0ad6bb745ee1035d4ba7ffc7296b507fde67ae86
Andrew <acasta4704@gmail.com>
2020-10-03 17:14:39 +1000

    project 10 complete

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,83 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = new Date();
+const dateFooter = document.querySelector(".date");
+dateFooter.textContent = date.getFullYear();
 
 // ********** close links ************
+const linkCont = document.querySelector(".links-container");
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+// hardcoded class change to show links container.
+// navToggle.addEventListener("click", () => {
+//   linkCont.classList.toggle("show-links");
+// });
+
+// Using JS to dynamically calc size and show
+navToggle.addEventListener("click", () => {
+  // get the height of the container (default hides links at 0 height)
+  const containerHeight = linkCont.getBoundingClientRect().height;
+  // get height of the list, within the above container that is being hidden
+  const linksHeight = links.getBoundingClientRect().height;
+  // check, if height is 0 (hidden) change height to fully open dynamically
+  if (containerHeight === 0) {
+    linkCont.style.height = `${linksHeight}px`;
+  } else {
+    linkCont.style.height = "";
+  }
+});
 
 // ********** fixed navbar ************
+const navbar = document.querySelector("#nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default scrolling
+    e.preventDefault();
+    // close links dropdown
+    linkCont.style.height = "";
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href");
+    // calculate heights
+    const element = document.querySelector(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const elementHeight = element.offsetTop;
+    let position = elementHeight - navHeight;
+    // if navbar isn't fixed when clicked, account for introduction of fixed nav in calculation
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    // if small screen links panel is open
+    const linkContainerHeight = linkCont.getBoundingClientRect().height;
+    if (linkContainerHeight > 82) {
+      // 82 just because we know the nav height is 82
+      // add the links height to account for the maths when links are open
+      position = position + linkContainerHeight;
+    }
+    // scroll to final position
+    window.scrollTo({ left: 0, top: position });
+  });
+});
